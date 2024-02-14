99223b34a9a402fe471920983c6d19f756239c71
Goobriel <goob@umich.edu>
2024-02-14 17:05:39 -0500

    Mid Term Project v2.0
    Added Some Java Script Functionality in Contact Us doc

--- scriptMidterm.js
+++ scriptMidterm.js
@@ -1,12 +1,21 @@
-/*konami code*/
+
 alert("JESUS LOVES ME!")
-let code;
 
-document.getElementById('drop-item').addEventListener('click', function() {
-        alert("Pressed")
+let count =0;
+
+document.getElementById('btn-submit').addEventListener('click', function() {
+
+    let name = document.getElementById('Name').value.trim();
+    let num = document.getElementById('Number').value.trim();
+    let mail = document.getElementById('Email').value.trim();
+    let sub = document.getElementById('Subject').value.trim();
+    let mes = document.getElementById('Contact-Message').value.trim();
+
+    console.log(name + " _ " + num + " _ " +  mail + " _ " + sub);
+
+    if (name.length === 0 || num.length === 0 || mail.length === 0 || sub.length === 0 || mes.length === 0) {
+        alert("Please fill in all fields");
+    } else {
+        alert("Message Submitted");
+    }
 });
-
-
-if (code = "up" + "up" + "down" + "down" + "left" + "right" + "left" + "right"){
-    alert("Ester Egg found! Enter Code: 'Konami' at check out for %50 off!")
-};
