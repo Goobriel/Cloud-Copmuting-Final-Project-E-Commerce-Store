b78bad7054923912b3261044b6d95b84b15bbe26
Goobriel <goob@umich.edu>
2024-02-14 15:23:22 -0500

    Mid Term Project v1.3

--- scriptMidterm.js
+++ scriptMidterm.js
@@ -1,12 +1,12 @@
 /*konami code*/
 alert("JESUS LOVES ME!")
 let code;
-function konami(){
-    document.getElementById('Up').addEventListener('click', function() {
+
+document.getElementById('drop-item').addEventListener('click', function() {
         alert("Pressed")
-    });
-    if (code = "up" + "up" + "down" + "down" + "left" + "right" + "left" + "right"){
-        alert("Ester Egg found! Enter Code: 'Konami' at check out for %50 off!")
-    };
-    document.getElementById('story').textContent = story;
+});
+
+
+if (code = "up" + "up" + "down" + "down" + "left" + "right" + "left" + "right"){
+    alert("Ester Egg found! Enter Code: 'Konami' at check out for %50 off!")
 };
