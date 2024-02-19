7ab52cff905715746f8ba666fb4c1cc188ecd562
Goobriel <goob@umich.edu>
2024-02-19 18:16:14 -0500

    MidTerm v2.3
    Added "validation" to subsrcibe

--- scriptMidterm.js
+++ scriptMidterm.js
@@ -1,7 +1,4 @@
 
-alert("JESUS LOVES ME!")
-
-
 /***** Contact Form ********/
 
 document.getElementById('btn-submit').addEventListener('click', function() {
@@ -9,34 +6,17 @@ document.getElementById('btn-submit').addEventListener('click', function() {
     let name = document.getElementById('Name').value.trim();
     let num = document.getElementById('Number').value.trim();
     let mail = document.getElementById('Email').value.trim();
-    let sub = document.getElementById('Subject').value.trim();
+    let subj = document.getElementById('Subject').value.trim();
     let mes = document.getElementById('Contact-Message').value.trim();
 
-    if (name.length === 0 || num.length === 0 || mail.length === 0 || sub.length === 0 || mes.length === 0) {
-        alert("Please fill in all fields");
-    } else {
+    let check = mail.includes("@");
+
+    if (name.length === 0 || num.length === 0 || mail.length === 0 || subj.length === 0 || mes.length === 0) {
+        alert("Please fill in all fields.");
+    } else if (check != true) {
+        alert("Please enter a valid Email.");
+    }
+    else {
         alert("Message Submitted");
     }
 });
-
-/***** Login Up ********/
-
-
-
-const cardLinks = document.querySelectorAll('.card-link');
-
-// Add event listener to each card link
-cardLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        // Prevent default link behavior (e.g., navigating to a new page)
-        event.preventDefault();
-
-        // Get the ID of the clicked link
-        let clickedId = event.target.id;
-        
-        // Add the clicked ID to the cartList array
-        cartList.push(clickedId);
-        
-        // Display the updated cartList array
-        console.log(cartList);
-    })});
