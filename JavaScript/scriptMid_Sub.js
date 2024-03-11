ffab4dc253d206b4dc0e077385ef0968d71d6a8f
Goobriel <goob@umich.edu>
2024-03-11 16:01:56 -0400

    V3.1.0
    Added shopping cart structure along with display and hide functionality

---
+++ JavaScript/scriptMid_Sub.js
@@ -0,0 +1,35 @@
+
+
+
+let cartclicked = 0;
+
+$( "#cart-icon" ).on( "click", function() {
+    cartclicked = cartclicked + 1;
+    console.log("amount of clicks" + cartclicked);
+    if (cartclicked%2 == 1){
+        $("#close").show( "slow");
+    }
+    else if (cartclicked%2 == 0){
+        $("#close").hide( "slow");
+    }
+}); /* Maybe try this instead so it isnt lagging behind the clicks
+var flip = 0;
+$( "button" ).on( "click", function() {
+  $( "p" ).toggle( flip++ % 2 === 0 );
+});
+*/
+
+
+/***** Subscribe to Newsletter ********/
+
+document.getElementById('btn-subscribe').addEventListener('click', function() {
+    let sub = document.getElementById('subscription').value.trim();
+    let check = sub.includes("@");
+    if (sub.length === 0 || check != true) {
+        alert("Please use a valid email");
+    } else {
+        alert("Subscribed to InsperShirt NewsLetter!");
+    }
+});
+
+
